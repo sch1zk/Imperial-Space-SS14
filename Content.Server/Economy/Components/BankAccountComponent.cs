3a579d2fe0815040048d666e4ee9a49dd417c9dc
sch1zk <nikitch2012@gmail.com>
2022-12-10 11:53:08 +0700

    BankAccount currency fix

--- Content.Server/Economy/Components/BankAccountComponent.cs
+++ Content.Server/Economy/Components/BankAccountComponent.cs
@@ -12,7 +12,7 @@ public sealed class BankAccountComponent : Component
     [ViewVariables(VVAccess.ReadWrite), DataField("balance")]
     public FixedPoint2 Balance { get; set; } = 0;
     [ViewVariables(VVAccess.ReadOnly), DataField("currencyType")]
-    public string CurrencyType { get; } = "SpaceCash";
+    public string CurrencyType { get; } = "SpaceCredits";
     public BankAccountComponent(int minBalance, int maxBalance)
     {
        var random = IoCManager.Resolve<IRobustRandom>();
